1a75fcbd54b28a911f992d3ed673deaa25abb452
elvislazar <elvislazar.tj@gmail.com>
2025-10-07 18:49:17 +0530

    Second commit

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-application-sigma.vercel.app/",
+  baseURL: "https://employee-application-sigma.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
